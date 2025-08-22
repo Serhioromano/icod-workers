a2cb7dd1ed7d91578d5db58c0a46a075787cde59
Serhioromano <serhioromano@outlook.com>
2025-08-22 17:24:26 +0500

    lets update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+    TEST_VAR: string,
+    KV: KVNamespace
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Cloudflare Workers!')
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/', async (c) => {
+    c.env.KV.put('key', 'value');
+    const value = await c.env.KV.get('key');
+    return c.text(`Env var: ${c.env.TEST_VAR} and -- ${value}`);
 })
 
 export default app
